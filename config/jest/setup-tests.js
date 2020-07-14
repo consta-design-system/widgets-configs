2e18e7e62ce6a37a152e528c6446b96535c7a3b0
Andrey Melnikov <andrey.melnikov@csssr.com>
2020-07-14 16:35:06 +0300

    GDC-565: Перейти с enzyme на react-test-renderer: gpn-configs

--- config/jest/setup-tests.js
+++ config/jest/setup-tests.js
@@ -10,12 +10,7 @@ const CONTEXT = `${process.cwd()}/node_modules/`
  */
 const requireWithContext = (moduleName) => require(CONTEXT + moduleName)
 
-const Enzyme = requireWithContext('enzyme')
-const Adapter = requireWithContext('enzyme-adapter-react-16')
-
-require('jest-enzyme')
 require('jest-extended')
 
-Enzyme.configure({ adapter: new Adapter() })
-
 global.React = requireWithContext('react')
+global.TestRenderer = requireWithContext('react-test-renderer')
