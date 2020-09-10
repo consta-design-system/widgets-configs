779f0f81c5088656e119b73eb72c62617e47d3e7
Ayaz Z <screamansqy@gmail.com>
2020-09-10 23:51:51 +0500

    GDC-755: Перенести репозитории в gazprom-neft

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ const SLASH = "[/\\\\]"
 
 module.exports = {
   moduleNameMapper: {
-    '\\.(jpg|jpeg|png|gif|webp|svg|ttf|woff|woff2|)$': '<rootDir>/node_modules/@csssr/gpn-configs/config/jest/mock-files.js',
+    '\\.(jpg|jpeg|png|gif|webp|svg|ttf|woff|woff2|)$': '<rootDir>/node_modules/@consta/widgets-configs/config/jest/mock-files.js',
     "\\.css$": "identity-obj-proxy",
     "@/(.*)": "<rootDir>/src/$1"
   },
@@ -14,13 +14,13 @@ module.exports = {
   },
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
-    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/@csssr/gpn-configs/config/jest/jsPreprocess.js"
+    "^.+\\.(js|jsx)$": "<rootDir>/node_modules/@consta/widgets-configs/config/jest/jsPreprocess.js"
   },
   setupFiles: [
     'core-js'
   ],
   setupFilesAfterEnv: [
-    '<rootDir>/node_modules/@csssr/gpn-configs/config/jest/setup-tests.js'
+    '<rootDir>/node_modules/@consta/widgets-configs/config/jest/setup-tests.js'
   ],
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{ts,tsx}",
